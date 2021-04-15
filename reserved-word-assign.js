a589d16f604af17daa406542190100b5a2293032
jeikei10 <jkhassan33@gmail.com>
2021-04-15 12:24:24 +0300

    Add files via upload

---
+++ reserved-word-assign.js
@@ -0,0 +1,16 @@
+//Reserved words in JavaScript
+  //They cannot use these reserved words as variables, labels, or function names.
+  //Eg : case,catch,class,const,continue,debugger,default,delete
+
+  //What unicode is vs ASCII
+ 
+// Automatic Semi-colon Insertion
+
+  // In JavaScript, a semicolon is automatically inserted when
+    
+  //  when the next line starts with code that breaks the current one 
+    //when the end of the source code file is reached
+    //when there is a return statement on its own line
+    //when there is a break statement on its own line
+    //when there is a throw statement on its own line
+    //when there is a continue statement on its own line
