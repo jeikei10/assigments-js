f9996f6c10724ad1d2881ecbe79639e97707c011
jeikei10 <jkhassan33@gmail.com>
2021-04-15 11:28:25 +0300

    Add files via upload

---
+++ number-assign.js
@@ -0,0 +1,17 @@
+// What is BigInt and how to use it
+  //  BigInt is a built-in object whose constructor returns a bigint primitive.It represent the whole numbers larger than 253 - 1
+// Convert the following values to numbers using any the 4 methods 
+// null  To Number
+    console.log(Number(null));
+  
+   //true to number 
+   console.log(Number(true))
+
+   //false to number 
+      console.log(Number(true))
+
+// Convert these two strings to number and sum them const string1 ="1000"; const string2 = "2000"
+    
+    string1 = Number("1000")
+    string2 = Number("2000")
+    console.log(string1 + string2);
