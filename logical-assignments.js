2b28ac619559bda07c8578967c365f5fcffe106c
jeikei10 <jkhassan33@gmail.com>
2021-04-15 10:53:52 +0300

    Add files via upload

---
+++ logical-assignments.js
@@ -0,0 +1,24 @@
+Assignment
+   // 1. What is Short circuiting and What is it used for?
+         Short circuit evaluation  works with the && and || logical operators. The expressions are evaluated left to right.
+         So, if the condition is met and the rest of the conditions won’t affect the already evaluated result, the expression          will short-circuit and return that result.
+
+   // 2. Demonstrate use of the Logical AND using the following comparison operators
+    // i. ===, >,>=,<,<=,!==
+
+     AND with === and <
+     const x = 10;
+     const y = 15;
+     console.log((x === 2) && (y < 10)); // false
+
+     AND with > and <=
+     
+      const x = 10;
+      const y = 15;
+      console.log((x > 2) && (y <= 15)); // true
+
+      AND with === and !==
+      
+     const x = 10;
+     const y = 15;
+     console.log((x === 10) && (y !== 15)); // false
