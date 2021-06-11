bcaf9215a770e84e2bd6bf55c5937c40bdd4bedf
jeikei10 <jkhassan33@gmail.com>
2021-06-11 10:40:56 +0300

    Update reserved-word-assign.js

--- reserved-word-assign.js
+++ reserved-word-assign.js
@@ -1,6 +1,6 @@
 //Reserved words in JavaScript
-  //They cannot use these reserved words as variables, labels, or function names.
-  //Eg : case,catch,class,const,continue,debugger,default,delete
+  They cannot use these reserved words as variables, labels, or function names.
+  Eg : case,catch,class,const,continue,debugger,default,delete
 
   //What unicode is vs ASCII
    Ascii chararacter encoding standard for electronic commununication which supports only 128 characters(use 7 bits to represent the characters)
@@ -9,13 +9,12 @@
    Unicode respesents letters of English, Arabic ,Greek and the like, mathematical sysmbols = historical scripts etc
  
 // Automatic Semi-colon Insertion
-    
 
   // In JavaScript, a semicolon is automatically inserted when
     
-  //  when the next line starts with code that breaks the current one 
-    //when the end of the source code file is reached
-    //when there is a return statement on its own line
-    //when there is a break statement on its own line
-    //when there is a throw statement on its own line
-    //when there is a continue statement on its own line
+   when the next line starts with code that breaks the current one 
+    when the end of the source code file is reached
+    when there is a return statement on its own line
+    when there is a break statement on its own line
+    when there is a throw statement on its own line
+    when there is a continue statement on its own line
