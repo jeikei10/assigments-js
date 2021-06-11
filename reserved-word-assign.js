1eeed4130ed68ad16f25d80f88dc23bcadd35dea
jeikei10 <jkhassan33@gmail.com>
2021-06-11 10:38:56 +0300

    Update reserved-word-assign.js

--- reserved-word-assign.js
+++ reserved-word-assign.js
@@ -3,8 +3,13 @@
   //Eg : case,catch,class,const,continue,debugger,default,delete
 
   //What unicode is vs ASCII
+   Ascii chararacter encoding standard for electronic commununication which supports only 128 characters(use 7 bits to represent the characters)
+   Unicode computing industry standard(uses 8bits,16bit, or 32bits) for consistency encoding and representction expressed in world writing system.
+   ASCII also represents lowercase letters(a-z),uppercase latters(A-Z)digits(0-9) and sysmbols.
+   Unicode respesents letters of English, Arabic ,Greek and the like, mathematical sysmbols = historical scripts etc
  
 // Automatic Semi-colon Insertion
+    
 
   // In JavaScript, a semicolon is automatically inserted when
     
